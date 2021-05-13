7aad6eeb27223b94c297d37f31ffac769eab2fcf
Reberthk <reberthkss@outlook.com>
2021-05-12 23:38:47 -0300

    Updated similar movie dataclass response

--- movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/SimilarMovieDataclasse.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/SimilarMovieDataclasse.kt
@@ -3,5 +3,5 @@ package com.example.movie_detail.Dataclasses
 import com.squareup.moshi.Json
 
 data class SimilarMovieDataclasse(
-    @field:Json(name = "genre_ids") val genreIds: List<String>
+    @field:Json(name = "id") val id: String
 )
