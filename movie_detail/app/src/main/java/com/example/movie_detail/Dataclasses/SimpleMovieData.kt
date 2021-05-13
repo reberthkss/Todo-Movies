09d5b212c636b2eb32679511394c37c314e549f2
Reberthk <reberthkss@outlook.com>
2021-05-13 08:17:29 -0300

    Added nullable assertion for simple movie data dataclass

--- movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/SimpleMovieData.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/SimpleMovieData.kt
@@ -1,6 +1,6 @@
 package com.example.movie_detail.Dataclasses
 
 data class SimpleMovieData(
-    var movieDetails: MovieDetailsDataclasse,
-    var similarMovies: List<MovieDetailsDataclasse>
+    var movieDetails: MovieDetailsDataclasse? = null,
+    var similarMovies: List<MovieDetailsDataclasse>? = null
 )
