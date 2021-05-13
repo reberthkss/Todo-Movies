f47192b2c52f50d5ca0b3f6ee6e01ba9680cdfa7
Reberthk <reberthkss@outlook.com>
2021-05-13 08:54:10 -0300

    Updated view model
    Fix view model

--- movie_detail/app/src/main/java/com/example/movie_detail/Models/TMDBViewModel.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Models/TMDBViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.movie_detail.Dataclasses.SimpleMovieData
+import com.example.movie_detail.Dataclasses.TMDBResourceConfig
 import com.example.movie_detail.Repositories.TMDBRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -12,7 +13,8 @@ import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
 class TMDBViewModel() : ViewModel() {
-    private val movieSimpleData: MutableLiveData<SimpleMovieData> = MutableLiveData(SimpleMovieData())
+    private val resourcesServerConfiguration: MutableLiveData<TMDBResourceConfig?> = MutableLiveData(null)
+    private val movieSimpleData: MutableLiveData<SimpleMovieData?> = MutableLiveData(null)
     private lateinit var theMovieDatabaseRepository: TMDBRepository
     private var isLoading: MutableLiveData<Boolean> = MutableLiveData(false);
 
@@ -20,7 +22,7 @@ class TMDBViewModel() : ViewModel() {
         theMovieDatabaseRepository = TMDBRepository(baseUrl, apiKey);
     }
 
-    public fun loadDataOfMovieId(movieId: String): Unit {
+    fun loadDataOfMovieId(movieId: String): Unit {
         // TODO - Error handling
         viewModelScope.launch {
             if (this@TMDBViewModel::theMovieDatabaseRepository.isInitialized) {
@@ -35,6 +37,17 @@ class TMDBViewModel() : ViewModel() {
             }
         }
     }
-    fun getMovieDetails(): LiveData<SimpleMovieData>{return movieSimpleData}
+
+    fun loadResourcesServerConfig() {
+        viewModelScope.launch {
+            if (this@TMDBViewModel::theMovieDatabaseRepository.isInitialized) {
+                val resourceServerConfig = theMovieDatabaseRepository.getResourcesConfiguration();
+                resourcesServerConfiguration.value = resourceServerConfig;
+            }
+        }
+    }
+
+    fun getMovieDetails(): LiveData<SimpleMovieData?>{return movieSimpleData};
     fun getLoadingStatus(): LiveData<Boolean> {return isLoading};
+    fun getResourceServerConfig(): LiveData<TMDBResourceConfig?> {return resourcesServerConfiguration};
 }
