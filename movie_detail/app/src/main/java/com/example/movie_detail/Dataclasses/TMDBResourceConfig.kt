2d0e9eab2a6257230b426ec90a8416b1f2deab4d
Reberthk <reberthkss@outlook.com>
2021-05-13 09:13:57 -0300

    Refactored The Movie Database configuration response management

--- movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/TMDBResourceConfig.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Dataclasses/TMDBResourceConfig.kt
@@ -3,5 +3,5 @@ package com.example.movie_detail.Dataclasses
 import com.squareup.moshi.Json
 
 data class TMDBResourceConfig(
-    @field:Json(name = "secure_base_url") val baseUrl: String
+    @field:Json(name = "images") val images: TMDBImagesConfig
 )
