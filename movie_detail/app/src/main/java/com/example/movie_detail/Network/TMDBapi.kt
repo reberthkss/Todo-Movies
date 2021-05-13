a807109d115724b4f5c222bcf341858540aca5bb
Reberthk <reberthkss@outlook.com>
2021-05-12 22:09:19 -0300

    Updated Get Similar Movies response

--- movie_detail/app/src/main/java/com/example/movie_detail/Network/TMDBapi.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Network/TMDBapi.kt
@@ -1,5 +1,6 @@
 package com.example.movie_detail.Network
 
+import com.example.movie_detail.Dataclasses.GetSimilarMoviesResponse
 import com.example.movie_detail.Dataclasses.MovieDetailsDataclasse
 import retrofit2.Call
 import retrofit2.http.GET
@@ -10,5 +11,5 @@ interface TMDBapi {
     @GET("movie/{movieId}")
     fun loadMovieId(@Path("movieId") movieId: String, @Query("api_key") apiKey: String): Call<MovieDetailsDataclasse>
     @GET("movie/{movieId}/similar")
-    fun loadSimilarMovies(@Path("movieId") movieId: String, @Query("api_key") apiKey: String): Call<Any>
+    fun loadSimilarMovies(@Path("movieId") movieId: String, @Query("api_key") apiKey: String): Call<GetSimilarMoviesResponse>
 }
