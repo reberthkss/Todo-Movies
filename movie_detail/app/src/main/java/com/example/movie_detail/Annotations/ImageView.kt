a84d048aa80cd1354997c93bef11742a96a34cb7
Reberthk <reberthkss@outlook.com>
2021-05-16 09:51:11 -0300

    updated image view  binding adapter

--- movie_detail/app/src/main/java/com/example/movie_detail/Annotations/ImageView.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Annotations/ImageView.kt
@@ -1,17 +1,25 @@
 package com.example.movie_detail.Annotations
 
+import android.util.Log
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
-@BindingAdapter("imageUrl")
-fun setImageUrl(imageView: ImageView, imageUrl: String?) {
+@BindingAdapter(value = ["imageUrl", "imageSize", "baseUrl"], requireAll = false)
+fun setImageUrl(imageView: ImageView, imageUrl: String?, imageSize: String?, resourcesBaseUrl: String?) {
+    val baseUrl = resourcesBaseUrl;
     if (imageUrl != null) {
-        val URI = imageUrl.toUri().buildUpon().scheme("https").build();
+        val imagePath = baseUrl + imageSize + imageUrl;
+        Log.d("BindingAdapter", "url => ${imagePath} ")
+        val URI = imagePath.toUri().buildUpon().scheme("https").build();
         Glide
             .with(imageView.context)
             .load(URI)
+            .apply(
+                RequestOptions()
+            )
             .into(imageView)
     }
 }
