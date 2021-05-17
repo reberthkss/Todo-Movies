3f2b9bdaa08165be5f6beb342102ad95a568ec26
Reberthk <reberthkss@outlook.com>
2021-05-17 20:55:38 -0300

    Added for the database model the cross reference

--- movie_detail/app/src/main/java/com/example/movie_detail/Room/Database.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Room/Database.kt
@@ -2,13 +2,17 @@ package com.example.movie_detail.Room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.movie_detail.Room.CrossReference.MovieAndGenreCfDao
 import com.example.movie_detail.Room.Entities.Genre.GenreEntity
 import com.example.movie_detail.Room.Entities.Genre.GenreEntityDao
 import com.example.movie_detail.Room.Entities.Movie.MovieEntity
 import com.example.movie_detail.Room.Entities.Movie.MovieEntityDao
 
-@Database(entities = arrayOf(MovieEntity::class, GenreEntity::class), version = 1)
+@Database(entities = arrayOf(MovieEntity::class, GenreEntity::class), version = 2)
 abstract class Database: RoomDatabase() {
     abstract fun movie(): MovieEntityDao
     abstract fun genre(): GenreEntityDao
+    abstract class CrossReferences {
+        abstract fun movieAndGenre(): MovieAndGenreCfDao
+    }
 }
