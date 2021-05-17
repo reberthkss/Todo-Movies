2ee92d650d275099b4ae2c27e846f5809c7315d3
Reberthk <reberthkss@outlook.com>
2021-05-17 19:52:35 -0300

    Implemented api interface

--- movie_detail/app/src/main/java/com/example/movie_detail/Network/TMDBapirefactor.kt
+++ movie_detail/app/src/main/java/com/example/movie_detail/Network/TMDBapirefactor.kt
@@ -1,4 +1,23 @@
 package com.example.movie_detail.Network
 
+import com.example.movie_detail.Dataclasses.GetSimilarMoviesResponse
+import com.example.movie_detail.Dataclasses.MovieDetailsDataclasse
+import com.example.movie_detail.Dataclasses.TMDBResourceConfig
+import com.example.movie_detail.Network.Movie.MovieApi
+import com.example.movie_detail.Network.Responses.GetGenres
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+
 interface TMDBapirefactor {
+    @GET("movie/{movieId}")
+    fun loadMovieById(@Path("movieId") movieId: String, @Query("api_key") apiKey: String): Call<MovieApi?>
+    @GET("movie/{movieId}/similar")
+    fun loadSimilarMovies(@Path("movieId") movieId: String, @Query("api_key") apiKey: String): Call<GetSimilarMoviesResponse?>
+    @GET("configuration")
+    fun getResourcesConfig(@Query("api_key") apiKey: String): Call<TMDBResourceConfig>
+    @GET("genre/movie/list")
+    fun getGenresList(@Query("api_key") apiKey: String): Call <GetGenres?>
 }
